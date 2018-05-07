8ed6036d73af37719198c05155cdc713a2c63e66
Apriliafauziaah <33899215+Apriliafauziaah@users.noreply.github.com>
2018-05-07 07:59:28 +0700

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-	<title>test</title>
+	<title>Project ADS</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
 	<script src="js-image-slider.js" type="text/javascript"></script>
@@ -15,18 +15,18 @@
 <h2> ( B A S A R N A S )<h2><div class="box3"><img src="callcenter.png" height="65px" width="170px"></img>
 <div id="cari">
 	<input name="search" type="text" placeholder="Search..." />
-	</div>
-<div id="akun"><img src="user.png" height="60px" width="55px">
+</div>
+<div id="akun"><a href="index.php">Login</a>
 </div>
 	
 	</div></div>
-<div id = "navigasi">
+<div id = "navigasi"><div id= "kotakmenu">
 			<div id = "menu1" ><a href="home.php">HOME</a></div>
-			<div id = "menu1" ><a href="nowplaying.php">SARPAS</a></div>
-			<div id = "menu1" ><a href="comingsoon.php">GALLERY</a></div>
+			<div id = "menu1" ><a href="Form Pendaftaran.php">PENDAFTARAN</a></div>
+			<div id = "menu1" ><a href="Galeri.php">GALLERY</a></div>
 			<div id = "menu1" ><a href="trailer.php">DISASTER</a></div>
 			<div id = "menu1" ><a href="theater.php">ABOUT US</a></div>
-	   </div> 
+	   </div> </div>
 
 
 <div class="content">
@@ -45,23 +45,42 @@
 <div class="contentberita">
 <div id="KOTAK"><div id="kotakTOP">BERITA TERKINI
 </div><div id="kotakDOWN">
-</div></div>
-<div id="berita1"><img src="9.jpg" height="300px" width="200px"></div>
-<div id="berita2"><img src="30.jpg" height="300px" width="200px"></div>
-<div id="berita3"><img src="8.jpg" height="300px" width="200px"></div>
+</div>
 
-<div id="KOTAKV"><div id="kotakTOP">VIDEO TERKINI
-</div><div id="kotakDOWN">
+<div id="berita1"><div class="gallery">
+  <a target="_blank" href="20.jpg">
+    <img src="20.jpg" alt="Forest" width="250" height="200">
+  </a>
+  <div class="desc">Add a description of the image here</div>
 </div></div>
-<div id="kotakvideo"></div>
+
+<div id="berita2"><div class="gallery">
+  <a target="_blank" href="8.jpg">
+    <img src="8.jpg" alt="Forest" width="250" height="200">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div></div>
+</div>
+
+
+
+<div id="KOTAKVideo"><div id="kotakTOP">VIDEO TERKINI
+</div><div id="kotakDOWN">
+</div>
+<div class="boxkanan">
+		<div class="kotakvideo">
+		<video id="frag1" controls autoplay width="430px" height="320px">
+		<source src="Penyelamatan.mp4"
+            type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'
+            data-original="Penyelamatan.mp4">
+		</video>
+		</div>
+		</div>
+</div>
 </div>
 <div class="footer">
-<div class="boxlogo"><div class="box4"><img src="1.png" height="78px" width="85px"></img></div><div class="box4a"><div class="bold">LPSE </div>Layanan Pengadaan Secara Elektronik</div></div>
-<div class="boxlogo"><div class="box4"><img src="4.png" height="78px" width="85px"></img></div><div class="box4a"><div class="bold">WEBMAIL </div>Layanan Pengadaan Secara Elektronik</div></div>
-<div class="boxlogoa"><div class="box4"><img src="2.png" height="78px" width="85px"></img></div><div class="box4a"><div class="bold">GIS </div>Layanan Pengadaan Secara Elektronik</div></div>
-<div class="boxlogob"><div class="box4"><img src="3.png" height="78px" width="85px"></img></div><div class="box4a"><div class="bold">LAPOR </div>Layanan Pengadaan Secara Elektronik</div></div>
-<div class="boxlogoc"><div class="box4"><img src="6.png" height="78px" width="85px"></img></div><div class="box4a"><div class="bold">SIMASDA</div>Layanan Pengadaan Secara Elektronik</div></div>
-<div class="boxlogod"><div class="box4"><img src="5.png" height="78px" width="85px"></img></div><div class="box4a"><div class="bold"> SIRUP </div>Layanan Pengadaan Secara Elektronik</div></div>
+<div id="kotakfooter"></div>
+
 </div>
 <div class="footer1">Copyright @ Badan Nasional Pencarian dan Pertolongan 2017 
 <br>[basarnas]</br></div>
