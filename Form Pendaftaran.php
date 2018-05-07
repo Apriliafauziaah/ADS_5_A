af55908a7774b8781364eee3d016342cdf6bcbd9
Apriliafauziaah <33899215+Apriliafauziaah@users.noreply.github.com>
2018-05-08 06:26:04 +0700

    Add files via upload

---
+++ Form Pendaftaran.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Project ADS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="form2.css">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body >
+<div class="boxbesar">
+<div class="header">
+	<div id="nama2"></div>
+	<div class="box3"><img src="callcenter.png" height="65px" width="170px"></img></div>
+	<div id="cari">
+		<input name="search" type="text" placeholder="Search..." /></div>
+	<div id="akun"><a href="index.php">Login</a></div>
+</div>
+
+	
+	
+<div id = "navigasi"><div id= "kotakmenu">
+			<div id = "menu1" ><a href="home.php">HOME</a></div>
+			<div id = "menu1" ><a href="Form Pendaftaran.php">PENDAFATARAN</a></div>
+			<div id = "menu1" ><a href="Galery.html">GALLERY</a></div>
+			<div id = "menu1" ><a href="trailer.php">DISASTER</a></div>
+			<div id = "menu1" ><a href="theater.php">ABOUT US</a></div>
+	   </div> </div>
+<div class="content">
+<div class="kotakdaf"></div>
+
+<form class="form-horizontal">
+<fieldset>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="nama">nama</label>  
+  <div class="col-md-4">
+  <input id="nama" name="nama" type="text" placeholder="nama" class="form-control input-md">
+  <span class="help-block">help</span>  
+  </div>
+</div>
+
+<!-- Select Basic -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="jenis kelamin">jenis kelamin</label>
+  <div class="col-md-2">
+    <select id="jenis kelamin" name="jenis kelamin" class="form-control">
+      <option value="1">pria</option>
+      <option value="2">wanita</option>
+    </select>
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="No HP/WA/BBM">No HP/WA/BBM</label>  
+  <div class="col-md-4">
+  <input id="No HP/WA/BBM" name="No HP/WA/BBM" type="text" placeholder="No HP/WA/BBM" class="form-control input-md">
+  <span class="help-block">help</span>  
+  </div>
+</div>
+
+<!-- File Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="pas foto">pas foto</label>
+  <div class="col-md-4">
+    <input id="pas foto" name="pas foto" class="input-file" type="file">
+  </div>
+</div>
+
+<!-- Textarea -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Alamat tinggal">Alamat tinggal</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="Alamat tinggal" name="Alamat tinggal">Alamat tinggal</textarea>
+  </div>
+</div>
+
+<!-- Textarea -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Alamat KTP">Alamat KTP</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="Alamat KTP" name="Alamat KTP">Alamat</textarea>
+  </div>
+</div>
+
+<!-- File Button --->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="KTP">KTP</label>
+  <div class="col-md-4">
+    <input id="KTP" name="KTP" class="input-file" type="file">
+  </div>
+</div>
+
+<!-- File Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="SIM">SIM</label>
+  <div class="col-md-4">
+    <input id="SIM" name="SIM" class="input-file" type="file">
+  </div>
+</div>
+
+<!-- File Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="STNK">STNK</label>
+  <div class="col-md-4">
+    <input id="STNK" name="STNK" class="input-file" type="file">
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="kirim"></label>
+  <div class="col-md-4">
+    <button id="kirim" name="kirim" class="btn btn-success">kirim</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+</div>
+
+<div class="footer">
+<div id="kotakfooter"></div>
+
+</div>
+<div class="footer1">Copyright @ Badan Nasional Pencarian dan Pertolongan 2017 
+<br>[basarnas]</br></div>
+</div>
+</body>
+</html>
